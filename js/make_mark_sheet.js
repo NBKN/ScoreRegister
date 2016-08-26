de974ba199b63b77ac0a6aefec9cc854be3bceed
NBKN <knt.watanab@gmail.com>
2016-08-26 19:59:24 +0900

    マークシートに基準点を設定&IDを設定してテストデータを表示できるようにした

--- js/make_mark_sheet.js
+++ js/make_mark_sheet.js
@@ -21,7 +21,7 @@ function createIdArea() {
 	var tableElement = document.createElement("table");
 	tableElement.className = 'mark_sheet';
 
-	var headerItem = '<tr class="row"><td class="marked_cell"></td><td colspan="5">会員番号</td><td class="marked_cell"></td></tr>';
+	var headerItem = '<tr class="row"><td id="begin_id" class="marked_cell"></td><td colspan="5">会員番号</td><td class="marked_cell"></td></tr>';
 	tableElement.innerHTML = headerItem;
 
 	memberInfo = makeTableRow('td class="mark_cell"', ' , , , , , , ',  'row');
@@ -36,6 +36,11 @@ function createIdArea() {
 	}
 	tableElement.innerHTML += memberInfo;
 	document.getElementById('id_area').appendChild(tableElement);
+//    var rect = document.getElementById('begin_id').getBoundingClientRect();
+//    console.log(rect.left + " " + rect.top);
+//    console.log(document.getElementById('id_area').getBoundingClientRect().left + " " + document.getElementById('id_area').getBoundingClientRect().top);
+//    document.getElementById('check_point').style.left = (rect.left) + 'px';
+//    document.getElementById('check_point').style.top = rect.top;
 }
 
 function createMarkArea() {
@@ -47,7 +52,7 @@ function createMarkArea() {
 
 	var mark_temple = '';
 	for (var select_cnt = 0; select_cnt < 10; select_cnt++) {
-		mark_temple += '<td class="mark_cell">' + dataArray[select_cnt] + '</td>'
+		mark_temple += '<td class="mark_cell">' + dataArray[select_cnt] + '</td>';
 	}
 
 	var q_cnt = 1;
@@ -57,11 +62,25 @@ function createMarkArea() {
 		markSheet.className = 'mark_sheet';
 		markSheet.innerHTML = '<tr class="row"><td>問</td><td class="marked_cell"></td><td colspan="8">解答記入欄</td><td class="marked_cell"></td></tr>';
 		for (var q = q_cnt; q < q_cnt + 20; q++) {
-			rows += '<tr class="row"><td class="mark_cell_index">' + q + '</td>' + mark_temple + '</tr>';
+			rows += '<tr class="row"><td class="mark_cell_index">' + q + '</td>';
+			for (var select_cnt = 0; select_cnt < 10; select_cnt++) {
+				rows += '<td class="mark_cell" id="' + q + '_' + dataArray[select_cnt] + '">' + dataArray[select_cnt] + '</td>';
+			}
+			+ '</tr>';
 		}
 		markSheet.innerHTML += rows;
 		document.getElementById('mark_sheet_area').appendChild(markSheet);
 		q_cnt += 20;
+	}
+
+	var test_data = [1,2,3,4,5,6,7,8,9,0,1,4,5,2,2,6,7,8];
+	var index = 1;
+
+	for (var i = 0; i < test_data.length; i++) {
+		var id = index + '_' + test_data[i];
+		console.log(id);
+		document.getElementById(id).className = 'marked_cell';;
+		index++;
 	}
 }
 

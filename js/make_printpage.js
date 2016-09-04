0163f30e6f0e8ee1b6e237ce1d3df33ecf87ef29
NBKN <knt.watanab@gmail.com>
2016-09-04 20:15:09 +0900

    クオーテーション修正

--- js/make_printpage.js
+++ js/make_printpage.js
@@ -1,18 +1,18 @@
 /**
- * プリント用データフォーマット [ "会員番号", "名前", "科目名1, 得点, 偏差値, 順位, 平均点, 中央値", "科目名2... ], ...
+ * プリント用データフォーマット [ '会員番号', '名前', '科目名1, 得点, 偏差値, 順位, 平均点, 中央値', '科目名2... ], ...
  */
 
 function doPrint() {
 	/* サンプルデータ */
 	var st_dataArray = [
-			[ "000", "name0", "英語,100,50,1,40,50", "国語,200,50,2,40,50",
-					"数1A,300,50,3,40,50", "数2B,400,50,4,40,50",
-					"物理,500,50,5,40,50", "化学,600,50,6,40,50",
-					"生物, 700, 50,7,40,50", "日本史,800,50,8,40,50",
-					"世界史, 900,50,9,40,50" ],
-			[ "001", "name1", "英語,100,50,1,40,50", "国語,200,50,2,40,50" ],
-			[ "002", "name2", "英語,100,50,1,40,50", "数1A,300,50,3,40,50",
-					"数2B,400,50,4,40,50", "生物, 700, 50,7,40,50" ] ];
+			[ '000', 'name0', '英語,100,50,1,40,50', '国語,200,50,2,40,50',
+					'数1A,300,50,3,40,50', '数2B,400,50,4,40,50',
+					'物理,500,50,5,40,50', '化学,600,50,6,40,50',
+					'生物, 700, 50,7,40,50', '日本史,800,50,8,40,50',
+					'世界史, 900,50,9,40,50' ],
+			[ '001', 'name1', '英語,100,50,1,40,50', '国語,200,50,2,40,50' ],
+			[ '002', 'name2', '英語,100,50,1,40,50', '数1A,300,50,3,40,50',
+					'数2B,400,50,4,40,50', '生物, 700, 50,7,40,50' ] ];
 
 	st_dataArray = makePrintData();
 	makePrintSection(st_dataArray);
@@ -23,7 +23,7 @@ function doPrint() {
  */
 function makePrintSection(st_dataArray) {
 	var checkBoxArray = handsonTable.getDataAtCol(0);
-	var article = document.getElementById("article");
+	var article = document.getElementById('article');
 	for (var i = 0; i < checkBoxArray.length; i++) {
 		var tmpName = st_dataArray[i]['name'];
 		var tmpSum = st_dataArray[i]['score'][getHeaderName('sum')];
@@ -31,7 +31,7 @@ function makePrintSection(st_dataArray) {
 			continue;
 		}
 		var st_data = st_dataArray[i];
-		var sectionElement = document.createElement("section");
+		var sectionElement = document.createElement('section');
 		sectionElement.className = 'print_page';
 		sectionElement.innerHTML = '<h1 class="print_title">成績表</h1>';
 
@@ -55,13 +55,13 @@ function makeMemberInfo(st_data) {
 	var name = st_data['name'];
 	var type = st_data['type'];
 
-	var headerItem = "番号,名前,文理区分";
-	headerItem = makeTableRow("th", headerItem);
+	var headerItem = '番号,名前,文理区分';
+	headerItem = makeTableRow('th', headerItem);
 	var rowItem = id + ',' + name + ',' + type;
-	rowItem = makeTableRow("td", rowItem);
+	rowItem = makeTableRow('td', rowItem);
 
 	var memberInfo = headerItem + rowItem;
-	var tableElement = document.createElement("table");
+	var tableElement = document.createElement('table');
 	tableElement.className = 'member_info';
 	tableElement.innerHTML = memberInfo;
 
@@ -72,17 +72,17 @@ function makeMemberInfo(st_data) {
  * 成績情報テーブル作成
  */
 function makeScoreInfo(scoreData) {
-	var scoreInfo = "科目,得点,偏差値,順位,平均,中央値";
-	scoreInfo = makeTableRow("th", scoreInfo);
+	var scoreInfo = '科目,得点,偏差値,順位,平均,中央値';
+	scoreInfo = makeTableRow('th', scoreInfo);
 
 	for (var key in scoreData) {
-		var rowItem = makeTableRow("td", scoreData[key]);
+		var rowItem = makeTableRow('td', scoreData[key]);
 		if (rowItem != null) {
 			scoreInfo += rowItem;
 		}
 	}
 
-	var tableElement = document.createElement("table");
+	var tableElement = document.createElement('table');
 	tableElement.className = 'score_info';
 	tableElement.innerHTML = scoreInfo;
 
@@ -93,15 +93,15 @@ function makeScoreInfo(scoreData) {
  * 各行を作成するメソッド
  */
 function makeTableRow(tag, item) {
-	var row = "<tr>";
-	var itemArray = item.split(",");
+	var row = '<tr>';
+	var itemArray = item.split(',');
 	if (itemArray[1] == '') {
 		return null;
 	}
 	for (var i = 0; i < itemArray.length; i++) {
-		row += "<" + tag + ">" + itemArray[i] + "</" + tag + ">";
+		row += '<' + tag + '>' + itemArray[i] + '</' + tag + '>';
 	}
-	row += "</tr>";
+	row += '</tr>';
 	return row;
 }
 
